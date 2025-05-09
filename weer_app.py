cf8f1232054fdd5328efb3fb7bc5ba003ea3cd7e
jaydenh-deltion <97096023@st.deltion.nl>
2025-05-09 14:41:11 +0200

    it is working

--- weer_app.py
+++ weer_app.py
@@ -1,25 +1,19 @@
+import requests
+import os
 from dotenv import load_dotenv
 load_dotenv()
-
-import os
-import requests
-## hide key 
 api_key = os.environ['API_KEY']
-
 city = input('please provide the city name:')
-
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
-
 response = requests.get(url)
-
 if response.status_code == 200:
     data = response.json()
     temp = data['main']['temp']
     celsius = temp - 273.15
-    print(f'Tempratuur: {celsius:.2f}°C')
     desc = data['weather'][0]['description']
+    print(f'Tempratuur: {celsius:.2f}°C')
     print(f'Omschrijfing: {desc}')
-else:
-    print('Fout bij het ophalen vand de gegevens. probeer het opnieuw.')
+else: 
+    print('Error retrieving data, please try again.')
 
  ## python3 weer_app.py
